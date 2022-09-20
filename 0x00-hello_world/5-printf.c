7be92d0bbcb6d1879707cf1b300c5972e8b38656
9409010617086 <prettymohlabe@gmail.com>
2022-09-20 06:03:09 -0700

    h

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,13 +5,14 @@ include <stdio.h>
 /**
  *main - Entry point
  *
- *Return: Always 0
+ *Return: Always 0 (Success)
  */
 int main(void)
 
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
 
+printf("with proper grammar, but the outcome is a piece of art,\n");
+			
 return (0);
 
 }
