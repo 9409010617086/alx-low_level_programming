3f03abc076930a307735caaf075aa19ff1c59fc0
9409010617086 <prettymohlabe@gmail.com>
2022-09-21 03:51:16 -0700

    p

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive _or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_reemaining_days(int month, int day, int year);
+int convert_day(int montha, int day);
+void print_times_table(int n);
+
+
+#endif
