8a3517d43f6aea3687f5b16bd393982d889f3237
9409010617086 <prettymohlabe@gmail.com>
2022-09-22 03:59:47 -0700

    k

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,23 +3,15 @@
                                                                    
 
 /**                                                                
- *
- *  * swap_int - Swaps the values of two integers.                    
- *
- *   * @a: The first integer to be swapped.                            
- *
- *    * @b: The second integer to be swapped.                           
- *
- *     */                                                                
+ * swap_int - Swaps the values of two integers.                    
+ * @a: The first integer to be swapped.                            
+ * @b: The second integer to be swapped.                           
+ */                                                                
 
 void swap_int(int *a, int *b)                                      
 
 {                                                                  
-
-	        int tmp = *a;                                              
-
-		        *a = *b;                                                   
-
-			        *b = tmp;                                                  
-
+int tmp = *a;                                              
+*a = *b;                                                   
+*b = tmp;                                                  
 }
