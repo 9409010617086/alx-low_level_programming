4b542e37fff229e5ba304f7168a3754a3f47567e
9409010617086 <prettymohlabe@gmail.com>
2022-09-22 10:28:30 -0700

    dg

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,9 @@
+#include "holberton.h"
+
 #include <stdio.h>
 
 int main(void)
+
 {
 
 	    char s1[98] = "Hello ";
@@ -8,17 +11,21 @@ int main(void)
 	        char s2[] = "World!\n";
 
 		    char *p;
-		    printf("%s\n", s1);
 
-		        printf("%s", s2);
 
-			    p = _strcat(s1, s2);
 
-			        printf("%s", s1);
+		        printf("%s\n", s1);
 
-				    printf("%s", s2);
+			    printf("%s", s2);
 
-				        printf("%s", p);
+			        p = _strcat(s1, s2);
 
-					    return (0);
+				    printf("%s", s1);
+
+				        printf("%s", s2);
+
+					    printf("%s", p);
+
+					        return (0);
+
 }
