774b2645f8c3857d450165bdab0572837d94e4a2
9409010617086 <prettymohlabe@gmail.com>
2022-09-22 05:30:03 -0700

    ol

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int i;
+printf ("infinite loop incoming :(\n"):
+
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ */
+printf("infinite loop avoided! \\0/\n");
+
+return (0);
+}
