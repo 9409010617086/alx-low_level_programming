74bf0f59754dce3715f71e9b14a2607820a1ddbd
9409010617086 <prettymohlabe@gmail.com>
2022-09-22 15:01:45 -0700

    t

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,17 +10,8 @@ int main(void)
 {
 
 	int i;
-
-
-
 	printf("Infinite loop incoming :(\n");
-
-
-
 	i = 0;
-
-
-
 	/*while (i < 10) */
 
 	/*{ */
@@ -28,12 +19,7 @@ int main(void)
 	/*      putchar(i);*/
 
 	/*} */
-
-
-
 	printf("Infinite loop avoided! \\o/\n");
-
-
 
 	return (0);
 }
