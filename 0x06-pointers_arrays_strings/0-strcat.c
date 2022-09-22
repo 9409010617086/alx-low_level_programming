2134fab67390ca8c7fb849cd3a898bdbd2c53248
9409010617086 <prettymohlabe@gmail.com>
2022-09-22 03:40:19 -0700

    ol

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-
-
-	char *_strcat(char *dest, char *src)
-	while (dest[index++])
-	dest_len++;
-	for (index = 0; src[index]; index++)
-	dest[dest_len++] = src[index];
-
-	return (dest);
+char *_strcat(char *dest, char *src)
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
