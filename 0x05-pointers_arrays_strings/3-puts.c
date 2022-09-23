b32bda9916b5e0065b31030ffa61f5e6fdf95793
9409010617086 <111237261+9409010617086@users.noreply.github.com>
2022-09-23 17:07:16 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,13 +4,13 @@
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
  */
-void puts(char *str)
+void _puts(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		putchar(str[i]);
+	_putchar(str[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
