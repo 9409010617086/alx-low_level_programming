982eba7fc983cc0336b608154cf2c858d3b65cf7
9409010617086 <111237261+9409010617086@users.noreply.github.com>
2022-09-23 21:37:51 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,13 +16,14 @@ void puts_half(char *str)
 		len++;
 	}
 
-	if (len % 2 == 0)
+if (len % 2 == 0)
 	{
 for (i = len / 2; str[i] != '\0'; i++)
 		{
 putchar(str[i]);
 		}
-	} else if (len % 2)
+	} 
+else if (len % 2)
 	{
 for (n = (len - 1) / 2; n < len - 1; n++)
 		{
