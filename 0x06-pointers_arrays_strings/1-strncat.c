26786e58b7f0cf6692842df9c9330a8d108cfa54
9409010617086 <prettymohlabe@gmail.com>
2022-09-23 03:59:49 -0700

    d

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,23 +3,15 @@
 
 
 /**
-
  * _strncat - concatenates n bytes from a string to another
-
  * @dest: destination string
-
  * @src: source string
-
  * @n: number of bytes of str to concatenate
-
  *
-
  * Return: a pointer to the resulting string dest
-
  */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
 
 	int i, j;
