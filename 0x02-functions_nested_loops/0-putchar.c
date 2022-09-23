601c86bd06cce7d102cadacbda697fc86c60c016
9409010617086 <prettymohlabe@gmail.com>
2022-09-23 04:21:07 -0700

    po

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,13 @@
 #include "main.h"
-
+#include <stdio.h>
 
 
 /**
-
  * main -  prints _putchar, followed by a new line
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
-
 {
 
 	_putchar('_');
