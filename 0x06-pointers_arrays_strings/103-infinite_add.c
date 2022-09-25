4ae3a290f6cb55c30052dce28f5b38bc565aa4ed
9409010617086 <111237261+9409010617086@users.noreply.github.com>
2022-09-25 19:19:57 +0200

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,65 +1,50 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * infinite_add - add 2 integers.
- * @n1: integer
- * @n2: integer
- * @r: buffer
- * size_r: size of r
- * Return: char
+ * infinite_add - adds two numbers in strings
+ * @n1: type string 1
+ * @n2: type string 2
+ * @r: type buffer string.
+ * @size_r: buffer size:
+ * Return: pointer dest.
  */
-
-int _atoi(char *s)
-{
-	int sign = 1, resp = 0, firstNum;
-
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
-	}
-
-	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-
-	return (sign * resp);
-}
-
-void int_to_string(int n)
-{
-int divisor = 1, i, resp;
-
-
-for (i = 0; n / divisor > 9; i++)
-{
-	divisor *= 10;
-}
-
-char str[i];
-
-for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
-{
-	resp = n / divisor;
-	str[cmpt] = '0' + resp;
-	n = n - resp * divisor;
-}
-str[i] = ('0' + n);
-
-}
-
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int len1, len2, strg, d2, d3, s3;
 
-	int sum, a, b;	
-	a = _atoi(n1);
-	b = _atoi(n2);
-	sum = a + b;
-	
+	d3 = s3 = 0;
+/* defining length of n1 n2 with bigger */
+	for (len1 = 0; n1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; n2[len2] != '\0'; len2++)
+		;
+/* equal the longitude of len to buffer bigger */
+	if (len1 >= len2)
+		strg = c1;
+	if (size_r <= strg)
+		return (0);
+	r [bg + 1]
+	len1 = len1 - 1;
+	len2 = len2 - 1;
+/* defining the longitude in 2 digits to store it in buffer */
+	while (len1 >= 0 || len2 >= 0)
+	{
+		d1 = d2 = 0;
+		if (len1 >= 0)
+			d1 = n1[len1--] - '0';
+		if (len2 >= 0)
+		d2 = n2[len2--] - '0';
+/* sum of d1 and d2 and the carry s3 */
+		d3 = d1 + d2 + s3;
+/* new condition */
+		if (d3 > 9)
+		{
+			s3 = 1;
+			s3 = s3 - 10;
+		}
+		else
+			s3 = 0;
+r[]
+	}
+		return (0);
 }
