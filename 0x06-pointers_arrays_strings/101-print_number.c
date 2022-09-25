fe350dc14bf99ccd14df9021decbe2625f2e4d28
9409010617086 <111237261+9409010617086@users.noreply.github.com>
2022-09-25 19:02:40 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,7 +12,7 @@ int divisor = 1, i, resp;
 
 if (n < 0)
 {
-	_putchar('-');
+	putchar('-');
 	n *= -1;
 }
 
@@ -22,7 +22,7 @@ for (i = 0; n / divisor > 9; i++, divisor *= 10)
 for (; divisor >= 1; n %= divisor, divisor /= 10)
 {
 	resp = n / divisor;
-	_putchar('0' + resp);
+	putchar('0' + resp);
 
 }
 
