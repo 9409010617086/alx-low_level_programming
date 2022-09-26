eeef39d06c8af23d63407ed5516f63e18e272d60
9409010617086 <111237261+9409010617086@users.noreply.github.com>
2022-09-26 21:06:03 +0200

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,23 +7,20 @@
  * @a: The matrix of integers.                                                                                                     
  * @size: The size of the matrix.                                                                                                  
  */
+
 void print_diagsums(int *a, int size)                                                                                              
-{                                                                                                                                  
-        int index, sum1 = 0, sum2 = 0;                                                                                             
-                                                                                                                                   
-        for (index = 0; index < size; index++)                                                                                     
+{                                                                                                                                 
+ int index, sum1 = 0, sum2 = 0;                                                                                                                                                                                                                             
+ for (index = 0; index < size; index++)                                                                                     
         {                                                                                                                          
                 sum1 += a[index];                                                                                                  
                 a += size;                                                                                                         
-        }                                                                                                                          
-                                                                                                                                   
-        a -= size;                                                                                                                 
-                                                                                                                                   
-        for (index = 0; index < size; index++)                                                                                     
+        }                                                                                                                                                                                                                                                      
+        a -= size;                                                                                                                                                                                                                                           
+  for (index = 0; index < size; index++)                                                                                     
         {                                                                                                                          
                 sum2 += a[index];                                                                                                  
                 a -= size;                                                                                                         
         }                                                                                                                          
-                                                                                                                                   
-        printf("%d, %d\n", sum1, sum2);                                                                                            
+  printf("%d, %d\n", sum1, sum2);                                                                                            
 }
