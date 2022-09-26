f440f528e9890b48e3d357564f7ecdd5e2b26eb8
9409010617086 <111237261+9409010617086@users.noreply.github.com>
2022-09-26 21:01:14 +0200

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,13 +7,12 @@
  * @n: max bytes to use                                                                                                                 
  * Return: dest                                                                                                                         
  */                                                                                                                                     
-                                                                                                                                        
-                                                                                                                                        
+                                                                                                                                                                                                                                                                                
 char *_memcpy(char *dest, char *src, unsigned int n)                                                                                    
 {                                                                                                                                       
         unsigned int i;                                                                                                                 
                                                                                                                                         
-        for (i = 0; n > 0 ; i++, n--)                                                                                                   
+ for (i = 0; n > 0 ; i++, n--)                                                                                                   
         {                                                                                                                               
                 dest[i] = src[i];                                                                                                       
         }                                                                                                                               
