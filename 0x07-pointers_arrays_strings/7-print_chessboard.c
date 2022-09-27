a36fd80661d0c00e7bad3079907bc5fac1c36ba2
9409010617086 <111237261+9409010617086@users.noreply.github.com>
2022-09-27 09:36:28 +0200

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,21 @@
-#include "main.h"                                                                                                                  
-                                                                                                                                   
-/**                                                                                                                                
- * print_chessboard - Prints a chessboard.                                                                                         
- * @a: The chessboard to be printed.                                                                                               
- */                                                                                                                                
-void print_chessboard(char (*a)[8])                                                                                                
-{                                                                                                                                  
-        int indx1, indx2;                                                                                                          
-                                                                                                                                   
-        for (indx1 = 0; a[indx1][7]; indx1++)                                                                                      
-        {                                                                                                                          
-                for (indx2 = 0; indx2 < 8; indx2++)                                                                                
-                        _putchar(a[indx1][indx2]);                                                                                 
-                                                                                                                                   
-                _putchar('\n');                                                                                                    
-        }                                                                                                                          
-}
+#include "main.h"
 
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to pieces to print
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
