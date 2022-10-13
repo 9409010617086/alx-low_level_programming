abd765a86f9ad616960123cf92073aeeebefe65b
9409010617086 <111237261+9409010617086@users.noreply.github.com>
2022-10-13 13:33:07 +0200

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,23 +8,17 @@
 *Return: 0 if n is null
 * or sum of parameters in other case
 */
-
 int sum_them_all(const unsigned int n, ...)
 {
-
 va_list li;
 int sum = 0;
 unsigned int i;
-
 va_start(li, n);
-
 if (n != 0)
 {
 for (i = 0; i < n; i++)
 sum += va_arg(li, int);
 }
-
 va_end(li);
 return (sum);
-
 }
