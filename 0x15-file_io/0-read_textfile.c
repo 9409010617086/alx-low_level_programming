f712b222e8fe36ab2cf011d423935e1d5c644a87
pretty mohlabe <111237261+9409010617086@users.noreply.github.com>
2022-10-31 19:20:34 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,54 +1,44 @@
-#include <stdio.h>
+#include "main.h"
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include "main.h"
-
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX STDOUT.
- * @filename: Name of file to open.
- * @letter: Number of char in file.
- * Return: ssize_t.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd; /* file descriptor */
-	ssize_t read_file, write_file;
-	char *buf;
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
-	buf = malloc(sizeof(char) * letters);
-
-	/* -1 implies an error in opening the file */
 	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-		free(buf);
+		close(fd);
 		return (0);
 	}
-
-	read_file = read(fd, buf, letters);
-
-	if (read_file == -1)
-	{
-		free(buf);
-		return (0);
-	}
-
+	lenr = read(fd, buffer, letters);
 	close(fd);
-
-	write_file = write(STDOUT_FILENO, buf, read_file);
-
-	if (write_file == -1)
+	if (lenr == -1)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
-
-	if (write_file != read_file)
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
 		return (0);
-	return (read_file);
+	return (lenw);
 }
