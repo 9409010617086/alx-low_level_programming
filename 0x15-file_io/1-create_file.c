4869e22e885d68bc19754e8449595f7e31e237ef
pretty mohlabe <111237261+9409010617086@users.noreply.github.com>
2022-10-31 19:21:11 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,43 +1,38 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
 /**
- * create_file - Creates a file.
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * @filename: Name of the file to create.
- * @text_content: NULL terminated string to write to the file.
- * Return: int.
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd; /* file descriptor */
-	int write_file;
-	int count = 0; /* For the size of text_content */
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	/* Create the file if it does not exist with rw-------, else
-	truncate ita with it current permissions. */
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	/* Add text_content to filename if it is not empty */
-	if (text_content != NULL)
-	{
-		while (*(text_content + count) != '\0')
-			count++;
+	if (!text_content)
+		text_content = "";
 
-		write_file = write(fd, text_content, count);
-	}
-	if (write_file == -1)
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
 
-	close (fd);
+	close(fd);
+
 	return (1);
 }
