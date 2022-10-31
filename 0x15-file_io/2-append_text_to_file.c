cd68d2c8d5273ccffa49284b5849e57a1cba6bb2
pretty mohlabe <111237261+9409010617086@users.noreply.github.com>
2022-10-31 19:24:14 +0200

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,39 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
 /**
- * append_text_to_file - Append_text_to_file.
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
  *
- * @filename:
- * @text_content:
- * Return: int 
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd; /* file descriptor */
-	int count = 0; /* To count the chars in  text_content */
-	int append_file;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-    
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
-	/* If there is an error fr = -1 */
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		while (*(text_content + count) != '\0')
-			count++;
-        
-		append_file = write(fd, text_content, count);
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
 	}
 
-	if (append_file == -1)
-		return (-1);
+	close(fd);
 
 	return (1);
 }
